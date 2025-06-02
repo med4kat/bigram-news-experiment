d401b9fbf53383cfa8a488ffa73a5a6532a87623
kat <tikvina.kat@gmail.com>
2025-06-02 17:43:08 +0100

    fixed correlation matrix

--- src/sentiment_vs_stock_analysis.py
+++ src/sentiment_vs_stock_analysis.py
@@ -11,13 +11,15 @@ with open("data/processed/stock_data.json", "r") as file:
 
 # Calculate average daily sentiment polarity
 sentiment_df = pd.DataFrame(sentiment_data)
-sentiment_df["date"] = pd.to_datetime(sentiment_df["headline"].str.extract(r'(\d{4}-\d{2}-\d{2})')[0])
+sentiment_df["date"] = pd.to_datetime(sentiment_df["timestamp"]).dt.date
+# sentiment_df["date"] = pd.to_datetime(sentiment_df["headline"].str.extract(r'(\d{4}-\d{2}-\d{2})')[0])
 daily_sentiment = sentiment_df.groupby("date")["polarity"].mean().reset_index()
 
 # Analyze stock data for a specific ticker (e.g., NVDA)
 ticker = "NVDA"
-stock_df = pd.DataFrame(stock_data[ticker])
-stock_df["Date"] = pd.to_datetime(stock_df["Date"])
+stock_df = pd.DataFrame(stock_data)
+
+stock_df["Date"] = pd.to_datetime(stock_df["Datetime"]).dt.date
 
 # Merge sentiment with stock data
 merged_data = pd.merge(daily_sentiment, stock_df, left_on="date", right_on="Date")
